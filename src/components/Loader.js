3cd54567e5e2bafae094fe55d611cb09c9a8b005
emad-salah <emads14@gmail.com>
2020-11-04 16:22:43 +0100

    Classname error fix

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -3,11 +3,11 @@ import React from "react";
 const Loader = ({ text = "Loading..." }) => (
   <div className="loading-wall">
     <div className="loading-wall-icon">
-      <span class="loading-circle loading-circle-1"></span>
-      <span class="loading-circle loading-circle-2"></span>
-      <span class="loading-circle loading-circle-3"></span>
-      <span class="loading-circle loading-circle-4"></span>
-      <span class="loading-circle loading-circle-5"></span>
+      <span className="loading-circle loading-circle-1"></span>
+      <span className="loading-circle loading-circle-2"></span>
+      <span className="loading-circle loading-circle-3"></span>
+      <span className="loading-circle loading-circle-4"></span>
+      <span className="loading-circle loading-circle-5"></span>
     </div>
     <div className="loading-wall-text">{text}</div>
   </div>

384b6b16f0839dbb1377530ce059fcc3ada2c25d
emad-salah <emads14@gmail.com>
2020-11-06 23:47:27 +0100

    Added SPA support for GitHub Pages

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import * as serviceWorker from "./serviceWorker";
 ReactDOM.render(
   <Provider store={store}>
     <React.StrictMode>
-      <Router>
+      <Router basename="/web-client">
         <App />
       </Router>
     </React.StrictMode>

cf967977f3c3bb85fc631b3647c5ca8323838b4b
emad-salah <emads14@gmail.com>
2021-05-18 15:04:54 +0100

    Lazy load webtorrent

--- src/utils/Torrents.js
+++ src/utils/Torrents.js
@@ -1,4 +1,3 @@
-import WebTorrent from "webtorrent";
 import { getCachedFile, renderCachedFile, saveFile } from "./Cache";
 import { runSerial } from "./Promise";
 
@@ -20,9 +19,9 @@ const supportedFileTypes = {
 
 const _getFileType = file => {
   const extension = file.name?.split(".")?.slice(-1)[0];
-  const supportedFileType = Object.entries(
-    supportedFileTypes
-  ).filter(([type, options]) => options.formats.includes(extension))[0];
+  const [supportedFileType] = Object.entries(supportedFileTypes).filter(
+    ([type, options]) => options.formats.includes(extension)
+  );
 
   if (supportedFileType) {
     const [name, fileType] = supportedFileType;
@@ -35,113 +34,132 @@ const _getFileType = file => {
   return null;
 };
 
-export const webTorrentClient = new WebTorrent();
+export let webTorrentClient = null;
 
-export const attachMedia = (posts = [], torrentMode = true) => {
+export const initializeClient = async () => {
+  if (!webTorrentClient) {
+    const WebTorrent = await import("webtorrent");
+    webTorrentClient = new WebTorrent.default();
+  }
+
+  return webTorrentClient;
+};
+
+export const attachMedia = async (posts = [], torrentMode = true) => {
   const torrentTasks = posts
     .map(post => {
       const { contentItems, id } = post;
-      return Object.entries(contentItems)
-        .filter(([key, item]) => supportedFileTypes[item.type])
-        .map(([key, item]) => () =>
-          new Promise(resolve => {
-            const torrentExists = webTorrentClient.get(item.magnetURI);
+      const supportedItems = Object.entries(contentItems).filter(
+        ([, item]) => supportedFileTypes[item.type]
+      );
 
-            if (torrentExists) {
-              resolve(true);
-              return;
-            }
+      if (!supportedItems) {
+        return [];
+      }
 
-            webTorrentClient.add(item.magnetURI, async torrent => {
-              // Proceed to the next torrent in queue
-              resolve(true);
+      return supportedItems.map(
+        ([key, item]) =>
+          () =>
+            new Promise(resolve => {
+              initializeClient().then(webTorrentClient => {
+                const torrentExists = webTorrentClient.get(item.magnetURI);
 
-              const files = torrent.files.filter(file => {
-                const extension = file.name?.split(".")?.slice(-1)[0];
-                const supportedFileType = Object.entries(
-                  supportedFileTypes
-                ).filter(([type, options]) =>
-                  options.formats.includes(extension)
-                )[0];
-                if (supportedFileType) {
-                  const [, fileType] = supportedFileType;
-                  const matched = fileType.formats.includes(extension);
-                  return matched;
-                }
-                return false;
-              });
-
-              files.map(async file => {
-                const fileType = _getFileType(file);
-
-                if (!fileType) {
+                if (torrentExists) {
+                  resolve(true);
                   return;
                 }
 
-                const fileName = `${id}-${key}-${file.name}`;
+                webTorrentClient.add(item.magnetURI, async torrent => {
+                  // Proceed to the next torrent in queue
+                  resolve(true);
 
-                const element = fileType.element;
-                const target = `${element}[data-torrent="${item.magnetURI}"]`;
-                const cachedFile = await getCachedFile(fileName);
+                  const files = torrent.files.filter(file => {
+                    const extension = file.name?.split(".")?.slice(-1)[0];
+                    const supportedFileType = Object.entries(
+                      supportedFileTypes
+                    ).filter(([, options]) =>
+                      options.formats.includes(extension)
+                    )[0];
+                    if (supportedFileType) {
+                      const [, fileType] = supportedFileType;
+                      const matched = fileType.formats.includes(extension);
+                      return matched;
+                    }
+                    return false;
+                  });
 
-                if (cachedFile) {
-                  const torrent = webTorrentClient.get(item.magnetURI);
+                  files.map(async file => {
+                    const fileType = _getFileType(file);
 
-                  if (torrent) {
-                    torrent.destroy();
-                  }
-
-                  renderCachedFile(cachedFile, target);
-                  return;
-                }
-
-                const torrentElements = document.querySelectorAll(target);
-                console.log("Torrent Elements:", torrentElements);
-                torrentElements.forEach(torrentElement => {
-                  const contentURL = decodeURIComponent(
-                    item.magnetURI.replace(/.*(ws=)/gi, "")
-                  );
-                  const [compatibleURL] = fileType.formats.filter(format =>
-                    contentURL
-                      .toLowerCase()
-                      .endsWith(`.${format.toLowerCase()}`)
-                  );
-
-                  if (torrentMode || !compatibleURL) {
-                    file.renderTo(torrentElement, fileType.options);
-                    return;
-                  }
-
-                  torrentElement.setAttribute("src", contentURL);
-                });
-              });
-
-              torrent.on("done", () => {
-                files.map(file => {
-                  const fileType = _getFileType(file);
-                  const fileName = `${id}-${key}-${file.name}`;
-                  const element = fileType.element;
-                  const target = `${element}[data-torrent="${item.magnetURI}"]`;
-
-                  file.getBlob(async (err, blob) => {
-                    if (err) {
-                      console.warn(err);
+                    if (!fileType) {
                       return;
                     }
 
-                    console.log("Caching loaded file...", fileName, blob);
-                    await saveFile(fileName, blob);
-                    const element = document.querySelector(target);
-                    if (element.dataset.played === "false") {
-                      const cachedFile = await getCachedFile(fileName);
+                    const fileName = `${id}-${key}-${file.name}`;
+
+                    const element = fileType.element;
+                    const target = `${element}[data-torrent="${item.magnetURI}"]`;
+                    const cachedFile = await getCachedFile(fileName);
+
+                    if (cachedFile) {
+                      const torrent = webTorrentClient.get(item.magnetURI);
+
+                      if (torrent) {
+                        torrent.destroy();
+                      }
+
                       renderCachedFile(cachedFile, target);
+                      return;
                     }
+
+                    const torrentElements = document.querySelectorAll(target);
+                    console.log("Torrent Elements:", torrentElements);
+                    torrentElements.forEach(torrentElement => {
+                      const contentURL = decodeURIComponent(
+                        item.magnetURI.replace(/.*(ws=)/gi, "")
+                      );
+                      const [compatibleURL] = fileType.formats.filter(format =>
+                        contentURL
+                          .toLowerCase()
+                          .endsWith(`.${format.toLowerCase()}`)
+                      );
+
+                      if (torrentMode || !compatibleURL) {
+                        file.renderTo(torrentElement, fileType.options);
+                        return;
+                      }
+
+                      torrentElement.setAttribute("src", contentURL);
+                    });
+                  });
+
+                  torrent.on("done", () => {
+                    files.map(file => {
+                      const fileType = _getFileType(file);
+                      const fileName = `${id}-${key}-${file.name}`;
+                      const element = fileType.element;
+                      const target = `${element}[data-torrent="${item.magnetURI}"]`;
+
+                      file.getBlob(async (err, blob) => {
+                        if (err) {
+                          console.warn(err);
+                          return;
+                        }
+
+                        console.log("Caching loaded file...", fileName, blob);
+                        await saveFile(fileName, blob);
+                        const element = document.querySelector(target);
+                        if (element.dataset.played === "false") {
+                          const cachedFile = await getCachedFile(fileName);
+                          renderCachedFile(cachedFile, target);
+                        }
+                      });
+                    });
                   });
                 });
               });
-            });
-          })
-        );
+            })
+      );
     })
     .reduce((torrents, contentItems) => [...torrents, ...contentItems], []);
 

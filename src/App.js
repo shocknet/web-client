d4b8e0f4c584fe8021f3f2aa7cc90370b3c139c6
emad-salah <emads14@gmail.com>
2021-09-03 12:46:15 +0100

    Added common meta tags

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { withRouter, Redirect, Route, Switch } from "react-router-dom";
 import Loader from "./common/Loader";
 import DownloadBtn from "./common/DownloadBtn";
 import "./styles/App.css";
+import { Helmet } from "react-helmet";
 
 const UserPage = React.lazy(() => import("./pages/User"));
 const PostPage = React.lazy(() => import("./pages/Post"));
@@ -10,6 +11,12 @@ const PostPage = React.lazy(() => import("./pages/Post"));
 function App() {
   return (
     <div className="App">
+      <Helmet>
+        <meta property="twitter:site" content="@shocknet" />
+        <meta property="twitter:card" content="summary" />
+        <meta property="og:url" content={window.location.href} />
+        <meta property="og:type" content="website" />
+      </Helmet>
       <Suspense fallback={<Loader />}>
         <Switch>
           <Route path="/user/:userId" component={UserPage} />

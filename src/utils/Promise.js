7a920fee75f4ad27d1faef8a7c138b72bc91ba7d
emad-salah <emads14@gmail.com>
2021-09-03 12:23:35 +0100

    Add crawler-specific await functionality

--- src/utils/Promise.js
+++ src/utils/Promise.js
@@ -1,7 +1,15 @@
+import { isCrawler } from "./Prerender";
+
 export const runSerial = tasks => {
   let result = Promise.resolve();
   tasks.forEach(task => {
     result = result.then(() => task());
   });
   return result;
+};
+
+export const crawlerAwait = async (...promises) => {
+  if (isCrawler()) {
+    return await Promise.all(promises);
+  }
 };

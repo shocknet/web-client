057eb74356f3d4e42d2d41fe2859f24c77a356e8
emad-salah <emads14@gmail.com>
2021-09-03 12:54:17 +0100

    Custom server linked to prerendering server

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
+const FS = require("fs");
 const Dotenv = require("dotenv");
 const Express = require("express");
 const Prerender = require("prerender-node");
-
+const Path = require("path");
 Dotenv.config();
+
 const app = Express();
 const port = process.env.PORT;
 
@@ -10,7 +12,28 @@ app
   .use(Prerender)
   .set("prerenderServiceUrl", process.env.PRERENDER_SERVICE_URL);
 
-app.use(Express.static("build"));
+const getStaticFilePath = filePath => Path.join(__dirname, "build", filePath);
+
+const fileExists = filePath =>
+  new Promise(resolve => {
+    FS.stat(getStaticFilePath(filePath), (err, stats) => {
+      if (err) {
+        resolve(false);
+        return;
+      }
+
+      resolve(stats.isFile);
+    });
+  });
+
+app.get(["*", "/"], async (req, res) => {
+  const staticFileExists = await fileExists(req.path);
+  if (staticFileExists && req.path !== "/") {
+    res.sendFile(getStaticFilePath(req.path));
+    return;
+  }
+  res.sendFile(getStaticFilePath("index.html"));
+});
 
 app.listen(port, () => {
   console.log(`Web-client listening at http://localhost:${port}`);
